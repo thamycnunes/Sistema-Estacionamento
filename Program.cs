aea2e3fc94d3b9e21ad7c1ddcc475808f35d6408
Thamires Nunes <thamires.cassia71@gmail.com>
2024-12-23 11:22:46 -0300

    Add files via upload
    "Desafio Concluído"

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using SistemaEstacionamento;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Seja bem-vindo ao sistema de estacionamento!");
+
+        Console.WriteLine("Digite o preço inicial:");
+        decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Digite o preço por hora:");
+        decimal precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+        Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+        bool exibirMenu = true;
+
+        while (exibirMenu)
+        {
+            Console.WriteLine("Digite a sua opção:");
+            Console.WriteLine("1 - Entrada de veículo");
+            Console.WriteLine("2 - Retirada de veículo");
+            Console.WriteLine("3 - Lista de veículos");
+            Console.WriteLine("5 - Encerrar");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    estacionamento.AdicionarVeiculo();
+                    break;
+
+                case "2":
+                    estacionamento.RemoverVeiculo();
+                    break;
+
+                case "3":
+                    estacionamento.ListarVeiculos();
+                    break;
+                
+                case "4":
+                    exibirMenu = false;
+                    Console.WriteLine("Encerrando o programa...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+            Console.WriteLine(); 
+        }
+    }
+}
---
+++ estacionamento.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaEstacionamento
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial;
+        private decimal precoPorHora;
+        private List<string> veiculos;
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+            this.veiculos = new List<string>();
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+            veiculos.Add(placa);
+            Console.WriteLine($"Veículo {placa} adicionado com sucesso!");
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Contains(placa))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                int horas = int.Parse(Console.ReadLine());
+
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+                veiculos.Remove(placa);
+
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente.");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Count > 0)
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                foreach (string veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+       
+    }
+}
